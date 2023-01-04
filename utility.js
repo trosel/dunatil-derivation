e2adc607a905e10f84eec17fdd9535378031140f
trosel <1299322+trosel@users.noreply.github.com>
2023-01-04 17:40:23 -0500

    Update utility.js

--- utility.js
+++ utility.js
@@ -2,160 +2,128 @@ function isVowel(x) {
     return ("aeiouAEIOU".indexOf(x) != -1); 
 }
 
-function getDerivatives(infinitive) {
+function getDerivatives(word, meaning) {
 
     var derivatives = [
         {
-        "infinitive": infinitive,
+        "infinitive": word,
         "formula": "[root]",
-        "derivative": "li " + perfectTheme + "e",
+        "derivative": word,
         "derivative type": "noun",
-        "explanation": "the / a " + infinitive,
-        "example": "dom = the / a house"
+        "explanation": "the / a " + word,
+        "example": "dom"
         },
         {
-        "infinitive": infinitive,
-        "form used": "perfect theme",
-        "formula": "perfect theme + ion" ,
-        "derivative": perfectTheme + "ion",
+        "infinitive": word,
+        "formula": "[root] + ik" ,
+        "derivative": word + "ik",
         "derivative type": "adjective",
-        "explanation": "[root]-like / [root]-ish / related to [root]",
-        "example": "colaboration"
+        "explanation": meaning + "-like / " + meaning + "-ish / related to " + meaning,
+        "example": "domik"
         },
         {
-        "infinitive": infinitive,
-        "form used": "perfect theme",
-        "formula": "perfect theme + or" ,
-        "derivative": perfectTheme + "or",
+        "infinitive": word,
+        "formula": "[root] + it" ,
+        "derivative": word + "it",
         "derivative type": "noun",
-        "explanation": "acting person, thing or factor",
-        "example": "administrator"
+        "explanation": meaning + "-ness / " + meaning "-ity / the quality or essence of " + meaning,
+        "example": "domit"
         },
         {
-        "infinitive": infinitive,
-        "form used": "perfect theme",
-        "formula": "perfect theme + ori" ,
-        "derivative": perfectTheme + "ori",
+        "infinitive": word,
+        "formula": "[root] + if" ,
+        "derivative": word + "if",
+        "derivative type": "noun",
+        "explanation": "an act of making " + meaning,
+        "example": "falif"
+        },
+        {
+        "infinitive": word,
+        "formula": "[root] + o" ,
+        "derivative": word + "o",
+        "derivative type": "adverb",
+        "explanation": "to act according to " + meaning,
+        "example": "noco"
+        },
+        {
+        "infinitive": word,
+        "formula": "[root] + oen" ,
+        "derivative": word + "oen",
+        "derivative type": "verb",
+        "explanation": "to do " + meaning + " / to do as " + meaning + " / to enact " + meaning,
+        "example": "tiloen"
+        },
+        {
+        "infinitive": word,
+        "formula": "[root] + ed + oen" ,
+        "derivative": word + "edoen",
+        "derivative type": "verb",
+        "explanation": "to do " + meaning + " / to do as " + meaning + " / to enact " + meaning + " in the past",
+        "example": "tiledoen"
+        },
+        {
+        "infinitive": word,
+        "formula": "[root] + ev + oen" ,
+        "derivative": word + "evoen",
+        "derivative type": "verb",
+        "explanation": "to do " + meaning + " / to do as " + meaning + " / to enact " + meaning + " in the future",
+        "example": "tilevoen"
+        },
+        {
+        "infinitive": word,
+        "formula": "[root] + oel" ,
+        "derivative": word + "oel",
+        "derivative type": "adjective (participle)",
+        "explanation": "tending to do " + meaning + " / tending to do as " + meaning + " / tending to enact " + meaning,
+        "example": "tiloel"
+        },
+        {
+        "infinitive": word,
+        "formula": "[root] + ed + oel" ,
+        "derivative": word + "edoel",
+        "derivative type": "adjective (participle)",
+        "explanation": "tended to do " + meaning + " / tended to do as " + meaning + " / tended to enact " + meaning,
+        "example": "tiloel"
+        },
+        {
+        "infinitive": word,
+        "formula": "[root] + ev + oel" ,
+        "derivative": word + "evoel",
+        "derivative type": "adjective (participle)",
+        "explanation": "intending to do " + meaning + " / intending to do as " + meaning + " / intending to enact " + meaning,
+        "example": "tiloel"
+        },
+        {
+        "infinitive": word,
+        "formula": "be + [root] + oen" ,
+        "derivative": "be" + word + "oen",
+        "derivative type": "verb",
+        "explanation": "to be the obect of " + meaning + " / to be " + meaning + "ed",
+        "example": "beloefoen"
+        },
+        {
+        "infinitive": word,
+        "formula": "[root] + ik + it" ,
+        "derivative": word + "ikit",
+        "derivative type": "noun",
+        "explanation": meaning + "ishness / the essense of being like a " + word,
+        "example": "domikit"
+        },
+        {
+        "infinitive": word,
+        "formula": "[root] + it + ik" ,
+        "derivative": word + "itik",
         "derivative type": "adjective",
-        "explanation": "destined toward, should act as (adjective of -or)",
-        "example": "administratori"
+        "explanation": "relating to " + meaning + "ness",
+        "example": "politik"
         },
         {
-        "infinitive": infinitive,
-        "form used": "perfect theme",
-        "formula": "perfect theme + iv" ,
-        "derivative": perfectTheme + "iv",
-        "derivative type": "adjective",
-        "explanation": "acting as such, capable of",
-        "example": "administrativ"
-        },
-        {
-        "infinitive": infinitive,
-        "form used": "perfect theme",
-        "formula": "perfect theme + ura" ,
-        "derivative": perfectTheme + "ura",
+        "infinitive": word,
+        "formula": "[root] + ik + if" ,
+        "derivative": word + "ikif",
         "derivative type": "noun",
-        "explanation": "concrete action or its result",
-        "example": "scritura"
-        },
-        {
-        "infinitive": infinitive,
-        "form used": "verbal root",
-        "formula": "verbal root + age" ,
-        "derivative": verbalRoot + "age",
-        "derivative type": "noun",
-        "explanation": "activity, mainly industrial or professional, also its result, location or time, also its expenses",
-        "example": "viage, espionage, portage"
-        },
-        {
-        "infinitive": infinitive,
-        "form used": "verbal root",
-        "formula": "verbal root + achar" ,
-        "derivative": verbalRoot + "achar",
-        "derivative type": "verb",
-        "explanation": "verb with negative, pejorative meaning",
-        "example": "plorachar"
-        },
-        {
-        "infinitive": infinitive,
-        "form used": "verbal root",
-        "formula": "verbal root + era" ,
-        "derivative": verbalRoot + "era",
-        "derivative type": "",
-        "explanation": "",
-        "example": ""
-        },
-        {
-        "infinitive": infinitive,
-        "form used": "verbal root",
-        "formula": "verbal root + ard" ,
-        "derivative": verbalRoot + "ard",
-        "derivative type": "noun",
-        "explanation": "people with a bad quality",
-        "example": "mentard"
-        },
-        {
-        "infinitive": infinitive,
-        "form used": "verbal root",
-        "formula": "verbal root + on" ,
-        "derivative": verbalRoot + "on",
-        "derivative type": "noun",
-        "explanation": "person characterized by an internal or natural quality",
-        "example": "novon"
-        },
-        {
-        "infinitive": infinitive,
-        "form used": "verbal root",
-        "formula": "verbal root + ore" ,
-        "derivative": verbalRoot + "ore",
-        "derivative type": "noun",
-        "explanation": "status or measurable quality",
-        "example": "calore"
-        },
-        {
-        "infinitive": infinitive,
-        "form used": "verbal root",
-        "formula": "verbal root + uore" ,
-        "derivative": verbalRoot + "uore",
-        "derivative type": "noun",
-        "explanation": "location or utensil",
-        "example": "reservuore"
-        },
-        {
-        "infinitive": infinitive,
-        "form used": "verbal root",
-        "formula": "verbal root + aci" ,
-        "derivative": verbalRoot + "aci",
-        "derivative type": "adjective",
-        "explanation": "tending towards something",
-        "example": "tending towards something"
-        },
-        {
-        "infinitive": infinitive,
-        "form used": "verbal root",
-        "formula": "verbal root + isar" ,
-        "derivative": verbalRoot + "isar",
-        "derivative type": "verb",
-        "explanation": "act in a manner / as",
-        "example": "terrorisar"
-        },
-        {
-        "infinitive": infinitive,
-        "form used": "verbal root",
-        "formula": "verbal root + ificar" ,
-        "derivative": verbalRoot + "ificar",
-        "derivative type": "verb",
-        "explanation": "act in a manner. also forms adjectives:: pacific, magnific, specific",
-        "example": "verificar"
-        },
-        {
-        "infinitive": infinitive,
-        "form used": "verbal root",
-        "formula": "verbal root + ear" ,
-        "derivative": verbalRoot + "ear",
-        "derivative type": "verb",
-        "explanation": "dynamic state of coming and going",
-        "example": "flammear"
+        "explanation": "an act of making " + meaning + "ish",
+        "example": "domikif"
         }
     ]
 
